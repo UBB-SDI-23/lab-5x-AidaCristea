76eebfe5b230bd77d749b7eb1dfdea81f48c5667
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2023-04-02 19:28:59 +0300

    Frontend

--- Frontend/MppFrontend/src/main.tsx
+++ Frontend/MppFrontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const GlobalURL="http://13.48.23.233";
+export const GlobalURL="http://16.170.210.14";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
