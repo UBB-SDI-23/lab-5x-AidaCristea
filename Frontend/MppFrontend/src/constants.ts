202c13e5d41c73e70d6a984b4bc3c74b14ccb265
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2023-04-18 14:29:33 +0300

    .

--- Frontend/MppFrontend/src/constants.ts
+++ Frontend/MppFrontend/src/constants.ts
@@ -3,4 +3,5 @@
 
 // export const BACKEND_API_URL =
 // 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
-export const BACKEND_API_URL="http://localhost:8080";
+//export const BACKEND_API_URL="http://localhost:8080";
+export const BACKEND_API_URL="/api";
